ee43c3951c1b7444b8a958107db9212eeb02415f
RaliD <rally_sl@abv.bg>
2019-06-20 15:14:50 +0100

    ePortfolio Updated

--- scripts/scriptJava.js
+++ scripts/scriptJava.js
@@ -1,5 +1,27 @@
 var aud = document.getElementById("audioName");
+var w;
 
 function playAudio() {
     aud.play();
+}
+
+playAudio();
+
+function websteros() {
+    if (typeof(Worker) !== "undefined") {
+        // Yes! Web worker support!
+        if (typeof(w) == "undefined") {
+            w = new Worker("./scripts/webWorker.js");
+            w.onmessage = function(event) {
+                document.getElementById("wwText").innerHTML = event.data;
+            };
+        }
+    } else {
+        // Sorry! No Web Worker support..
+    }
+}
+
+function stopWorker() {
+    w.terminate();
+    w = undefined;
 }
